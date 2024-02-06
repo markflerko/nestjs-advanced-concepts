ea7ff89873e81ce24a9873880231dcc5d30322c0
markflerko <markflerko@gmail.com>
2024-02-06 15:28:20 +0300

    schematics

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CronModule } from './cron/cron.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
 import { HttpClientModule } from './http-client/http-client.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { SchedulerModule } from './scheduler/scheduler.module';
     HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
     HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
     HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    RecipesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
